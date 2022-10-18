d0a0a4280992aed14a2ab338677d0067efc580e2
danRamirez91 <danii.rj91@gmail.com>
2022-10-17 19:45:37 -0500

    Cambios 7:45

--- src/Clases/Campo.java
+++ src/Clases/Campo.java
@@ -5,4 +5,48 @@ package Clases;
 class Campo {
     String nombre;
     String descripcion;
+    Tipo tipo;
+    Dato dato;
+
+    public Campo(String nombre, String descripcion, Tipo tipo) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Dato getDato() {
+        return dato;
+    }
+
+    public void setDato(Dato dato) {
+        this.dato = dato;
+    }
+    
+    
+    
+    
 }
